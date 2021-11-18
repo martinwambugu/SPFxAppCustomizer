6d12120c4518e46c34474d44f7f3338f00f206ee
Martin Wambugu <martin.wambugu@gmail.com>
2021-11-18 11:51:32 +0300

    18th Nov commit

--- src/extensions/helloAppCustomizer/HelloAppCustomizerApplicationCustomizer.ts
+++ src/extensions/helloAppCustomizer/HelloAppCustomizerApplicationCustomizer.ts
@@ -1,9 +1,15 @@
 import { override } from "@microsoft/decorators";
 import { Log } from "@microsoft/sp-core-library";
-import { BaseApplicationCustomizer } from "@microsoft/sp-application-base";
+import {
+  BaseApplicationCustomizer,
+  PlaceholderContent,
+  PlaceholderName,
+} from "@microsoft/sp-application-base";
 import { Dialog } from "@microsoft/sp-dialog";
 
 import * as strings from "HelloAppCustomizerApplicationCustomizerStrings";
+import styles from "./HelloAppCustomizerApplicationCustomizer.module.scss";
+import { escape } from "@microsoft/sp-lodash-subset";
 
 const LOG_SOURCE: string = "HelloAppCustomizerApplicationCustomizer";
 
@@ -14,22 +20,102 @@ const LOG_SOURCE: string = "HelloAppCustomizerApplicationCustomizer";
  */
 export interface IHelloAppCustomizerApplicationCustomizerProperties {
   // This is an example; replace with your own property
-  testMessage: string;
+  header: string;
+  footer: string;
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
 export default class HelloAppCustomizerApplicationCustomizer extends BaseApplicationCustomizer<IHelloAppCustomizerApplicationCustomizerProperties> {
+  private _topPlaceholder: PlaceholderContent | undefined;
+  private _bottomPlaceholder: PlaceholderContent | undefined;
+
   @override
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
 
-    let message: string = this.properties.testMessage;
-    if (!message) {
-      message = "(No properties were provided.)";
-    }
-
-    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
+    this.context.placeholderProvider.changedEvent.add(
+      this,
+      this._renderPlaceHolders,
+    );
 
     return Promise.resolve();
+  }
+
+  private _onDispose(): void {
+    console.log(
+      "[HelloWorldApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.",
+    );
+  }
+
+  private _renderPlaceHolders(): void {
+    console.log(
+      "Available application customizer placeholders: ",
+      this.context.placeholderProvider.placeholderNames
+        .map((name) => PlaceholderName[name])
+        .join(", "),
+    );
+
+    //obtain a handle to the top placeholder
+    if (!this._topPlaceholder) {
+      this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Top,
+        { onDispose: this._onDispose },
+      );
+
+      if (!this._topPlaceholder) {
+        console.error("The expected placeholder (Top) was not found.");
+        return;
+      }
+
+      if (this.properties) {
+        let headerMessage: string = this.properties.header;
+        if (!headerMessage) {
+          headerMessage = "(header property was not defined.)";
+        }
+
+        if (this._topPlaceholder.domElement) {
+          this._topPlaceholder.domElement.innerHTML = `
+            <div class="${styles.app}">
+              <div class="${styles.top}">
+                <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> ${escape(
+                  headerMessage,
+                )}
+              </div>
+            </div>`;
+        }
+      }
+    }
+
+    //Update the bottom placeholder
+    if (!this._bottomPlaceholder) {
+      this._bottomPlaceholder =
+        this.context.placeholderProvider.tryCreateContent(
+          PlaceholderName.Bottom,
+          { onDispose: this._onDispose },
+        );
+
+      if (!this._bottomPlaceholder) {
+        console.error("The expected placeholder (Bottom) was not found.");
+        return;
+      }
+
+      if (this.properties) {
+        let footerMessage: string = this.properties.footer;
+        if (!footerMessage) {
+          footerMessage = "(footer property was not defined.)";
+        }
+
+        if (this._bottomPlaceholder.domElement) {
+          this._bottomPlaceholder.domElement.innerHTML = `
+            <div class="${styles.app}">
+              <div class="${styles.bottom}">
+                <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> ${escape(
+                  footerMessage,
+                )}
+              </div>
+            </div>`;
+        }
+      }
+    }
   }
 }
